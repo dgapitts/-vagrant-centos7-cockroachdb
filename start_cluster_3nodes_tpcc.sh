37635d372f9db71eee67d3b67c4becb0b736b35e
Dave Pitts <dgapitts@gmail.com>
2021-08-08 20:35:41 +0200

    new script to setup / start crdb cluster with 3nodes and load tpcc

---
+++ start_cluster_3nodes_tpcc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pkill -9 cockroach
+# https://www.cockroachlabs.com/docs/v20.2/start-a-local-cluster
+# node1
+
+# remove previous cockroach setup 
+rm -rf node*
+mkdir -p node1
+mkdir -p node2
+mkdir -p node3
+
+cockroach start --insecure --background --store=node1 --listen-addr=localhost:26257 --http-addr=localhost:8080 --join=localhost:26257,localhost:26258,localhost:26259
+cockroach start --insecure --background --store=node2 --listen-addr=localhost:26258 --http-addr=localhost:8081 --join=localhost:26257,localhost:26258,localhost:26259
+cockroach start --insecure --background --store=node3 --listen-addr=localhost:26259 --http-addr=localhost:8082 --join=localhost:26257,localhost:26258,localhost:26259
+cockroach init --insecure --host=localhost:26257
+
+cockroach sql --execute="SET CLUSTER SETTING server.time_until_store_dead = '1m15s';" --insecure
+
+#cockroach workload init movr
+cockroach workload fixtures import tpcc --warehouses=10 'postgresql://root@localhost:26257?sslmode=disable'
+
+sleep 1; cockroach node status --host localhost:26257 --insecure
